6ee47ffab008dc422af4ffe7c71688651da9d738
tiendat <tiendat201228@gmail.com>
2021-11-03 21:27:29 +0700

    repo

--- django/test/runner.py
+++ django/test/runner.py
@@ -27,7 +27,6 @@ from django.test.utils import (
 )
 from django.utils.crypto import new_hash
 from django.utils.datastructures import OrderedSet
-from django.utils.version import PY37
 from django.utils.deprecation import RemovedInDjango50Warning
 
 try:
@@ -667,15 +666,14 @@ class DiscoverRunner:
                 'Output timings, including database set up and total run time.'
             ),
         )
-        if PY37:
-            parser.add_argument(
-                '-k', action='append', dest='test_name_patterns',
-                help=(
-                    'Only run test methods and classes that match the pattern '
-                    'or substring. Can be used multiple times. Same as '
-                    'unittest -k option.'
-                ),
-            )
+        parser.add_argument(
+            '-k', action='append', dest='test_name_patterns',
+            help=(
+                'Only run test methods and classes that match the pattern '
+                'or substring. Can be used multiple times. Same as '
+                'unittest -k option.'
+            ),
+        )
 
     @property
     def shuffle_seed(self):
